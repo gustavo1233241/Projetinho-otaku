1faacc1ac92393e832ed4c051f4cfe22359eaa1f
gustavo1233241 <128637314+gustavo1233241@users.noreply.github.com>
2024-05-23 17:13:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,31 +5,183 @@ var Fechar_OffCavas = document.getElementById('fecha_Box')
 
 function btnOffcanvas() {
 
-
     if (interruptor) {
 
         box_OffCavas.style.width = '30vw';
         box_OffCavas.style.display = 'block';
-        
-     
+        Fechar_OffCavas.style.display = 'block'
+
         interruptor = false
 
 
     }
-
-
     else {
         box_OffCavas.style.width = '0vw';
         box_OffCavas.style.display = 'none';
-        
+        Fechar_OffCavas.style.display = 'none'
+
 
         interruptor = true
 
-
     }
-    
-
-
-
 
 }
+
+var estrela1 = document.getElementById('estrela1')
+var estrela2 = document.getElementById('estrela2')
+var estrela3 = document.getElementById('estrela3')
+var estrela4 = document.getElementById('estrela4')
+var estrela5 = document.getElementById('estrela5')
+var estrela6 = document.getElementById('estrela6')
+var estrela7 = document.getElementById('estrela7')
+var estrela8 = document.getElementById('estrela8')
+var estrela9 = document.getElementById('estrela9')
+var estrela10 = document.getElementById('estrela10')
+
+function estrelas() {
+    if (interruptor) {
+        estrela2.style.color = 'rgb(45, 46, 41)';
+        estrela3.style.color = 'rgb(45, 46, 41)';
+        estrela4.style.color = 'rgb(45, 46, 41)';
+        estrela5.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas() {
+    if (interruptor) {
+        estrela2.style.color = 'yellow';
+        estrela3.style.color = 'yellow';
+        estrela4.style.color = 'yellow';
+        estrela5.style.color = 'yellow';
+        interruptor = true
+    }
+}
+function estrelas2() {
+    if (interruptor) {
+        estrela3.style.color = 'rgb(45, 46, 41)';
+        estrela4.style.color = 'rgb(45, 46, 41)';
+        estrela5.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas2() {
+    if (interruptor) {
+        estrela3.style.color = 'yellow';
+        estrela4.style.color = 'yellow';
+        estrela5.style.color = 'yellow';
+        interruptor = tru
+    }
+}
+function estrelas3() {
+    if (interruptor) {
+        estrela4.style.color = 'rgb(45, 46, 41)';
+        estrela5.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas3() {
+
+    if (interruptor) {
+        estrela4.style.color = 'yellow';
+        estrela5.style.color = 'yellow';
+        interruptor = true
+ }
+}
+function estrelas4() {
+
+    if (interruptor) {
+        estrela5.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas4() {
+    if (interruptor) {
+        estrela5.style.color = 'yellow';
+        interruptor = true
+    }
+}
+
+function estrelas5() {
+
+    if (interruptor) {
+        estrela6.style.color = 'rgb(45, 46, 41)';
+        estrela7.style.color = 'rgb(45, 46, 41)';
+        estrela8.style.color = 'rgb(45, 46, 41)';
+        estrela9.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas5() {
+    if (interruptor) {
+        estrela6.style.color = 'yellow';
+        estrela7.style.color = 'yellow';
+        estrela8.style.color = 'yellow';
+        estrela9.style.color = 'yellow';
+        interruptor = true
+    }
+}
+
+function estrelas6() {
+
+    if (interruptor) {
+       
+        estrela7.style.color = 'rgb(45, 46, 41)';
+        estrela8.style.color = 'rgb(45, 46, 41)';
+        estrela9.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas6() {
+    if (interruptor) {
+        estrela7.style.color = 'yellow';
+        estrela8.style.color = 'yellow';
+        estrela9.style.color = 'yellow';
+        interruptor = true
+    }
+}
+
+function estrelas7() {
+
+    if (interruptor) {
+       
+      
+        estrela8.style.color = 'rgb(45, 46, 41)';
+        estrela9.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas7() {
+    if (interruptor) {
+        
+      
+        estrela8.style.color = 'yellow';
+        estrela9.style.color = 'yellow';
+        interruptor = true
+    }
+}
+
+function estrelas8() {
+
+    if (interruptor) {
+       
+      
+        
+        estrela9.style.color = 'rgb(45, 46, 41)';
+        interruptor = true
+    }
+}
+function Tira_estrelas8() {
+    if (interruptor) {
+        
+      
+    
+        estrela9.style.color = 'yellow';
+        interruptor = true
+    }
+}
+
+
+
+
+
+
