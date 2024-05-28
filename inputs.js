7847df4811b8bfa5516b1d0045c93ebfa2682247
gu <gusera4325@gmail.com>
2024-05-28 17:10:57 -0300

    a

---
+++ inputs.js
@@ -0,0 +1,11 @@
+var input_manga = document.getElementById('pesquisa').value
+var tbate = 'tbate.html'
+function btn_pesquisa(){
+if(input_manga = 'tbate'){
+
+
+window.location.href = tbate
+
+
+}
+}
